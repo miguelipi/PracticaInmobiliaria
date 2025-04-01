2c17d94587dfab173dec145b479efc4171086efa
LauraAre <lauritadefuensa@gmail.com>
2025-04-01 10:37:24 +0200

    Register+LoginEnServidor

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,16 @@ app.get('/main.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/templates/main.html'));
 });
 
+app.get('/register.html', (req, res) => {
+    console.log("Petición a /register.html");
+    res.sendFile(path.join(__dirname, '../frontend/templates/register.html'));
+});
+
+app.get('/login.html', (req, res) => {
+    console.log("Petición a /login.html");
+    res.sendFile(path.join(__dirname, '../frontend/templates/login.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });

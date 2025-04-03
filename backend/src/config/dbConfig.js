28eb4b740e027a4c7e75fe49dfce69c007948d24
CdeDiego03 <145452771+CdeDiego03@users.noreply.github.com>
2025-04-03 11:28:20 +0200

    Index2

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     user: 'ADMIN',
     password: '!PsTVqiybf_THL5',
-    connectionString: 'jdbc:oracle:thin:@eurozona_high?TNS_ADMIN=C:\Users\Carlos\Documents\wallet'
+    connectionString: ''
   };
   
   async function testConnection() {
